756275985f338c3b84ec7199609dd52fd598fe80
jackpot9800 <sebastien@pointnet.tech>
2025-06-20 15:19:11 -0400

    Correction de l'erreur TypeScript Expo

--- metro.config.js
+++ metro.config.js
@@ -2,7 +2,14 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Configuration simplifiée pour éviter les conflits TypeScript
+// Configuration pour résoudre les problèmes TypeScript avec Expo 53
 config.resolver.sourceExts = ['js', 'jsx', 'ts', 'tsx', 'json'];
+config.resolver.assetExts = ['png', 'jpg', 'jpeg', 'gif', 'svg'];
+
+// Résoudre les problèmes avec les modules TypeScript
+config.transformer.babelTransformerPath = require.resolve('metro-react-native-babel-transformer');
+
+// Configuration pour les modules TypeScript
+config.resolver.platforms = ['native', 'web', 'ios', 'android'];
 
 module.exports = config;
